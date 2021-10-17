7385481383f973ef9eeab460e12ce3d97ab1cc9a
dewisnf <dewifadilah2@gmail.com>
2021-10-17 21:45:23 +0700

    mengerjakan modul pertemuan 2 membuat contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Dewi Siti Nur Fadilah
+ Saya tinggal di daerah Purbaratu
+ olah raga yang saya sukai adalah
+ Basket";
+ }
+}
